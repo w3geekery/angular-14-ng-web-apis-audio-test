8c049b3393fb3f93d78209ffe6578cc265d56ca6
Clark Stacer <cstacer@neverfail.com>
2022-09-28 08:02:37 -0700

    add @ng-web-apis/audio to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { WebAudioModule } from '@ng-web-apis/audio';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    WebAudioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
